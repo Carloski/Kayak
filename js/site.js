3770a71fcf73eed2e7f355ac49157fe037b1150e
Carloski <carlosmtamayo@gmail.com>
2021-09-12 22:24:56 -0400

    Started JS

--- js/site.js
+++ js/site.js
@@ -1,4 +1,21 @@
-//Testing Prism
-function helloWorld(){
-    alert('Hello World!');
+// Obtain users input
+function getValue() {
+    let word = document.getElementById("userInput").value;
+    //let results = document.getElementById("msg");
+    let returnMSG = isPalindrome(word);
+    showResults(returnMSG);
+}
+
+// Determine if input is a palindrome
+function isPalindrome(word) {
+ if(word.length > 4) {
+     showResults(word)
+ } else {
+     console.log("Not good");
+ }
+}
+
+//Display results
+function showResults() {
+    console.log(word);
 }
